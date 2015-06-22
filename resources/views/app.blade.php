5088f3bce73d63f52f09d801e60f9d5c20c1023d
gojo75 <leo.herisson@epitech.eu>
2015-06-22 17:06:42 +0200

    headbar changes #1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>Bloggus</title>
 
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('/css/bloggus.css') }}" rel="stylesheet">
+	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('/css/bloggus.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -17,65 +17,70 @@
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-</head>
-<body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">BLOGGUS</a>
-			</div>
+		<![endif]-->
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle Navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="/">
+						<img src="img/bloggus_logo_small.png" alt="bloggus logo">
+					</a>
+				</div>
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="{{ url('/') }}"></a></li>
-				</ul>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a href="{{ url('/') }}"></a></li>
+					</ul>
 
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
+					<ul class="nav navbar-nav navbar-right">
+						@if (Auth::guest())
 						<li><a href="{{ url('login') }}">Se connecter</a></li>
 						<li><a href="{{ url('register') }}">S'enregistrer</a></li>
-					@else
+						@else
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->username }}<span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+								<li><a href="/profile">Mon profil</a></li>
+								<li><a href="#">Gérer mes blogs</a></li>
+								<!-- <ul class="dropdown-menu" role="menu">
+									<li>Blog1</li>
+									<li>Blog1</li>
+									<li>Blog1</li>
+								</ul> -->
+								
+								<li><a href="#">Messages</a></li>
+								<li><a href="#">Mes amis</a></li>
+								<li class="divider"></li>
+								<li><a href="{{ url('/auth/logout') }}">Déconnexion</a></li>
 							</ul>
 						</li>
-					@endif
-				</ul>
+						@endif
+					</ul>
+				</div>
 			</div>
+		</nav>
+		<div class="col-md-3 bloggus-sidebar">
+			<ul class="nav nav-pills nav-stacked">
+
+				<div class="container-fluid">
+
+				</div>
+			</ul>
 		</div>
-	</nav>
-        <div class="col-md-3 bloggus-sidebar">
-            <ul class="nav nav-pills nav-stacked">
+		<div class="container">
+			@yield('content')
+		</div>
 
-            <div class="container-fluid">
-                <li><a href="#">Mon profile</a></li>
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Gérer mes blogs</a>
-                <ul class="dropdown-menu" role="menu">
-                    <li>Blog1</li>
-                    <li>Blog1</li>
-                    <li>Blog1</li>
-                </ul>
-                <li><a href="#">Messagerie</a></li>
-                <li><a href="#">Mes amis</a></li>
-            </div>
-            </ul>
-        </div>
-    <div class="container">
-	@yield('content')
-    </div>
-
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    <script src="{{ asset('js/ajaxupload.js') }}"></script>
-</body>
-</html>
+		<!-- Scripts -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<script src="{{ asset('js/ajaxupload.js') }}"></script>
+	</body>
+	</html>
