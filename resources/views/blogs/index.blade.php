85add5170f69fd7092d8fc06e4a2c8afaf65c9fc
gojo75 <leo.herisson@epitech.eu>
2015-06-25 19:19:25 +0200

    énième commit css

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,34 +1,42 @@
 @extends('app')
 
 @section('content')
-    @if(Auth::user()->blogs()->count() == 0)
-        <div class="col-md-10 col-md-offset-1">
-         <div id="no-blog-yet">
-             <div class="panel panel-default">
-                 <div class="panel-heading">Je suis nul en design</div>
-                 <div class="panel-body">
-                     <a href="{{route('blog.new')}}">Pas encore de blog ? Créer ton premier blog !</a>
-                 </div>
-                 <div class="panel-footer">Bonjour Léo change mon apparence stp </div>
-             </div>
-         </div>
-        </div>
-    @endif
+<div class="col-md-8">
+    <a href="{{route('blogs.new')}}">     
+        <button type="button" class="btn btn-default new-blog-button">
+            <i class="fa fa-plus"></i><span> Créer un blog</span>
+        </button>
+    </a>
+</div>
+@if(Auth::user()->blogs()->count() == 0)
+<div class="col-md-10 col-md-offset-1">
+   <div id="no-blog-yet">
+       <div class="panel panel-default">
+           <div class="panel-heading">Je suis nul en design</div>
+           <div class="panel-body">
+               <a href="{{route('blogs.new')}}">Pas encore de blog ? Créer ton premier blog !</a>
+           </div>
+           <div class="panel-footer">Bonjour Léo change mon apparence stp </div>
+       </div>
+   </div>
+</div>
+@endif
 
-        <div class="panel panel-default myblogs">
-            <div class="panel-heading myblogs-title">Mes blogs</div>
-                <div class="myblogs-thumbnails">
-                    <a href="{{route('blogs.index')}}"><img src="img/benz.jpg"></a>
-                </div>
-                @foreach($blogs as $blog)
-                <div class="panel-body myblogs-name">
-                    <a href="{{route('blog.dashboard',['id' => $blog->id])}}">
-                        <span class="myblogs-nametitle">{{$blog->title}}</span><br/>
-                        <span class="myblogs-descr">{{$blog->description}}</span>
-                    </a>
-                </div>
-                    @endforeach
-            </div>
-        </div>
-        
+
+<div class="panel panel-default myblogs">
+    <div class="panel-heading myblogs-title">Mes blogs</div>
+    <div class="myblogs-thumbnails">
+        <a href="{{route('blogs.index')}}"><img src="img/benz.jpg"></a>
+    </div>
+    @foreach($blogs as $blog)
+    <div class="panel-body myblogs-name">
+        <a href="{{route('blog.dashboard',['id' => $blog->id])}}">
+            <span class="myblogs-nametitle">{{$blog->title}}</span><br/>
+            <span class="myblogs-descr">{{$blog->description}}</span>
+        </a>
+    </div>
+    @endforeach
+</div>
+</div>
+
 @endsection
--- resources/views/userblog/dashboard.blade.php
+++ resources/views/userblog/dashboard.blade.php
@@ -7,36 +7,51 @@
         <div class="blog-thumbnail">
             <img src="{{asset('img/benz.jpg')}}" class="">
         </div>
+        <div id="blog-description" class="well well-lg blog-description" contenteditable="true">
+            {{$blog->description}}
+        </div>
+        <div class="edit-tools" style="display: block;">
+            <button class="edit-profile-confirm">Modifier</button>
+            <meta name="csrf-token" content="ngsiQgBP5rz0SobrxxzVQaN2P94Qd4WSTvaX1SwC">
+        </div>
         <h3>Infos du blog</h3>
         <div class="bloginfos">
-            <span class="bloginfos-created-at">créé le <strong>{{$blog->created_at}}</strong></span><br/>            
-            <span class="bloginfos-articles"><strong>12</strong> articles</span><br/>
-            <span class="bloginfos-commentaires"><strong>8</strong> commentaires</span><br/>
+            <i class="fa fa-calendar"></i><span class="bloginfos-created-at">créé le <strong>{{$blog->created_at}}</strong></span><br/>            
+            <i class="fa fa-pencil-square"></i><span class="bloginfos-articles"><strong>12</strong> articles</span><br/>
+            <i class="fa fa-comment"></i><span class="bloginfos-commentaires"><strong>8</strong> commentaires</span><br/>
+            <i class="fa fa-plus"></i><span class="bloginfos-abonnés"><strong>1 289 345</strong> abonnés</span><br/>
 
 
         </div>
     </div>
 <div class="editblog-row">
-	<div class="col-md-8">
-            <a href="{{route('blogs.articles.create',['id' => $id])}}">        
-			<button onclick="location.href='#'" type="button" class="btn btn-default post-article-button" href="#">
+	<div class="col-md-5">
+        <a href="{{route('blogs.articles.create',['id' => $id])}}">        
+			<button type="button" class="btn btn-default post-article-button">
 				<i class="fa fa-pencil-square-o"></i><span>Poster un article</span>
 			</button>
-            </a>
+        </a>
 	</div>
+    <div class="col-md-3">
+        <a href="#">        
+            <button type="button" class="btn btn-default blog-settings-button">
+                <i class="fa fa-cog"></i><span>Modifier le blog</span>
+            </button>
+        </a>
+    </div>
 </div>
 
 <div class="editblog-last-row">
 	<div class="col-md-9">
-		 <div class="panel panel-default myblogs">
-            <div class="panel-heading myblogs-title">Mes articles</div>
-                <div class="myblogs-thumbnails">
+		 <div class="panel panel-default myarticles">
+            <div class="panel-heading myarticles-title">Mes articles</div>
+                <div class="myarticles-thumbnails">
                     <a href="#"><img src="{{ asset('img/kamehameha.png') }}"></a>
                 </div>
-                <div class="panel-body myblogs-name">
+                <div class="panel-body myarticles-name">
                     <a href="#">
-                        <span class="myblogs-article-title">(TUTO) Comment faire un kamehameha</span><br/>
-                        <span class="myblogs-article-descr">Bonjour aujourd'hui on va faire du pain à la semoule...</span>
+                        <span class="myarticles-article-title">(TUTO) Comment faire un kamehameha</span><br/>
+                        <span class="myarticles-article-descr">Bonjour aujourd'hui on va faire du pain à la semoule...</span>
                     </a>
                     <br/><a href="#" class="modify-link"><i class="fa fa-pencil fa-2x"></i>MODIFIER</a>
                 </div>
