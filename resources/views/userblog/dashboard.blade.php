cde9da3321b1ac33c7ee0bcaa03f1b7d39d98a02
gojo75 <leo.herisson@epitech.eu>
2015-06-25 13:40:17 +0200

    asset

--- resources/views/userblog/dashboard.blade.php
+++ resources/views/userblog/dashboard.blade.php
@@ -18,7 +18,7 @@
 		 <div class="panel panel-default myblogs">
             <div class="panel-heading myblogs-title">Mes articles</div>
                 <div class="myblogs-thumbnails">
-                    <a href="#"><img src="img/benz.jpg"></a>
+                    <a href="#"><img src="{{ asset('img/benz.jpg') }}"</a>
                 </div>
                 <div class="panel-body myblogs-name">
                     <a href="#">
