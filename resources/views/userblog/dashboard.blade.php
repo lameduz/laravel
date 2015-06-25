ece296a29e5203ff5ac495ab8316e7ef56d61cf6
gojo75 <leo.herisson@epitech.eu>
2015-06-25 16:52:35 +0200

    best commit ever

--- resources/views/userblog/dashboard.blade.php
+++ resources/views/userblog/dashboard.blade.php
@@ -3,24 +3,26 @@
 @section('content')
 
     <div class="col-md-3 edit-blog-sidebar">
-        <h3>Infos du blog</h3>
-        <h1></h1>
+        <h1>{{$blog->title}}</h1>
         <div class="blog-thumbnail">
             <img src="{{asset('img/benz.jpg')}}" class="">
         </div>
+        <h3>Infos du blog</h3>
         <div class="bloginfos">
-            <span class="bloginfos-created-at">créé le {{$blog->created_at}}</span><br/>            
-            <span class="bloginfos-articles">12 articles</span><br/>
-            <span class="bloginfos-commentaires">8 commentaires</span><br/>
+            <span class="bloginfos-created-at">créé le <strong>{{$blog->created_at}}</strong></span><br/>            
+            <span class="bloginfos-articles"><strong>12</strong> articles</span><br/>
+            <span class="bloginfos-commentaires"><strong>8</strong> commentaires</span><br/>
 
 
         </div>
     </div>
 <div class="editblog-row">
 	<div class="col-md-8">
+            <a href="{{route('blogs.articles.create',['id' => $id])}}">        
 			<button onclick="location.href='#'" type="button" class="btn btn-default post-article-button" href="#">
-				<i class="fa fa-pencil-square-o"></i><span><a href="{{route('blogs.articles.create',['id' => $id])}}">Poster un article</span>
+				<i class="fa fa-pencil-square-o"></i><span>Poster un article</span>
 			</button>
+            </a>
 	</div>
 </div>
 
@@ -29,7 +31,7 @@
 		 <div class="panel panel-default myblogs">
             <div class="panel-heading myblogs-title">Mes articles</div>
                 <div class="myblogs-thumbnails">
-                    <a href="#"><img src="{{ asset('img/benz.jpg') }}"></a>
+                    <a href="#"><img src="{{ asset('img/kamehameha.png') }}"></a>
                 </div>
                 <div class="panel-body myblogs-name">
                     <a href="#">
